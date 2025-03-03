eba406bad66ae284e39e9994a4225909dff4b35e
AymanA <aliayman@alumni.harvard.edu>
2025-03-03 13:38:58 +0000

    create site

---
+++ reflections-react-code/src/components/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <div className="App">
+        <header className="App-header">
+            <title>My reflections</title>
+        </header>
+        </div>
+    );
+    }
